0ffa025b92acae80f191835c823392ef911f4f62
unknown <suwai2196@gmail.com>
2021-07-07 01:42:34 +0630

    change

--- app.py
+++ app.py
@@ -1,4 +1,11 @@
-from subbot import app
+from flask import Flask, render_template
+app = Flask(__name__)
+
+from subbot import routes
+
+app.config['SECRET_KEY'] = '8cb90f8fea491d6444248698'
+
+from subbot import functions
 
 if __name__=='__main__':
     app.run(debug=True)
--- subbot/__init__.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask,render_template
-app = Flask(__name__)
-
-
-from subbot import routes
-app.config['SECRET_KEY'] = '8cb90f8fea491d6444248698'
-
-from subbot import functions
